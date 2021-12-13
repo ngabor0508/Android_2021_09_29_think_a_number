55378d131d3b69eacf957de93fc16aeb34d53950
Nagy Gabor <ngabor0508@gmail.com>
2021-12-13 17:51:48 +0100

    Projekt létrehozva.

---
+++ app/src/test/java/com/nagy_gabor_2021_09_29_think_a_number/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.nagy_gabor_2021_09_29_think_a_number;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
